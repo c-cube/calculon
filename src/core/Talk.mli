3674bd86c52d2b99d51aafd69db64179ee779f70
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-01-21 21:32:13 +0100

    add some doc

--- src/core/Talk.mli
+++ src/core/Talk.mli
@@ -1,6 +1,12 @@
 
+(** {1 Basic Messages}
+
+    These messages are typical replies to user-issued commands, with a bit of
+    randomness to diminish the monotony *)
+
 type t =
-  | Ack
-  | Err
+  | Ack (** Ok *)
+  | Err (** Error occurred *)
 
 val select : t -> string
+(** A particular representation of this message *)
