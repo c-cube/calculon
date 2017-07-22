394046cd320ebc320ace2ad0e9cb2abf47efc9ac
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-07-22 08:53:03 +0200

    compat 4.01

--- src/core/Plugin_factoids.ml
+++ src/core/Plugin_factoids.ml
@@ -151,13 +151,12 @@ let set ({key;_} as f) (fcs:t): t =
   StrMap.add key f fcs
 
 let append {key;value} (fcs:t): t =
-  let value' =
-    match (StrMap.find key fcs).value, value with
-    | Int i, Int j -> Int (i+j)
-    | StrList l, StrList l' -> StrList (l @ l')
-    | StrList l, Int j -> StrList (string_of_int j :: l)
-    | Int i, StrList l -> StrList (string_of_int i :: l)
-    | exception Not_found -> value
+  let value' = match try Some ((StrMap.find key fcs).value, value) with Not_found -> None with
+    | Some (`Int i, Int j) -> Int (i+j)
+    | Some (`StrList l, StrList l') -> StrList (l @ l')
+    | Some (`StrList l, Int j) -> StrList (string_of_int j :: l)
+    | Some (`Int i, StrList l) -> StrList (string_of_int i :: l)
+    | None -> value
   in
   StrMap.add key {key; value = value'} fcs
 
