77e85dc491daf93daa47c58e12c02548189a2d2a
Antoine Pouille <antoine.pouille@datadoghq.com>
2018-04-08 20:07:19 +0200

    adding choice function "choix"

---
+++ src/core/Plugin_choice.mli
@@ -0,0 +1,8 @@
+
+(** {1 Choice plugin} *)
+
+(** Returns a random choice between two entries separated by a certain identifier
+    Example usage: '!choix manger dormir' or '!choix faire la lessive vs coder du coucoubot'
+    Linked tags: productivity, performance, automation, machine learning *)
+
+val plugin : Plugin.t
