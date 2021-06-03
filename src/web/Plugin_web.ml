54ced4a130929779d2474685be0912a311983d1d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-06-03 16:27:48 -0400

    emoji

--- src/web/Plugin_web.ml
+++ src/web/Plugin_web.ml
@@ -162,44 +162,42 @@ module Giphy = struct
       )
 end
 
-let cmd_emoji =
-  let find_h1 body =
-    let ast = Soup.parse body in
-    Soup.select "article h1" ast
-    |> Soup.to_list
-    |> CCList.take 1
-    |> CCList.map
-      (fun n ->
-         Soup.select "#emoji" n |> Soup.to_list
-         |> List.map Soup.to_string |> String.concat " ",
-         Soup.children n |> Soup.to_list |> CCList.take 1
-         |> List.map Soup.to_string |> String.concat " ")
-    |> CCList.head_opt
-  and find_search body =
-    let ast = Soup.parse body in
-    ast
-    |> Soup.select "article #search-results li a"
-    |> Soup.to_list |> CCList.take 1
-    |> CCList.find_map (Soup.attribute "href")
+let find_emoji (s:string) : string option Lwt.t =
+  let find_h1 ast =
+    let open CCOpt.Infix in
+    let open Soup in
+    let* h1 = ast $? "article h1" in
+    let+ em = h1 $? ".emoji" >>= Soup.leaf_text
+    and+ descr = h1 |> children |> last >>= leaf_text in
+    em, descr
+  and find_search ast  =
+    let open CCOpt.Infix in
+    let* a = ast $? ".search-results > li a" in
+    let+ href = Soup.attribute "href" a in
+    "https://emojipedia.org/"^ href
   in
 
+  let query = Printf.sprintf "https://emojipedia.org/search/?q=%s" s in
+  Log.debug (fun k->k "emoji: query is '%s'" query);
+  Lwt.catch
+    (fun () ->
+       get_body (Uri.of_string query) >|= fun body ->
+       let ast = Soup.parse body in
+       match find_h1 ast, find_search ast with
+       | Some (em,title), _ ->
+         Some (Printf.sprintf "%s: %s (%s)" em title query)
+       | None, Some href ->
+         Some href
+       | None, None -> Some (Printf.sprintf "not found"))
+    (fun e ->
+       Log.err (fun k->k "emoji: query failed:@.%s" (Printexc.to_string e));
+       Lwt.return None)
+
+let cmd_emoji =
   Command.make_simple ~descr:"look for emojis" ~cmd:"emoji" ~prio:10
     (fun _msg s ->
        let s = String.trim s in
-       let query = Printf.sprintf "https://emojipedia.org/search/?q=%s" s in
-       Log.debug (fun k->k "emoji: query is '%s'" query);
-       Lwt.catch
-         (fun () ->
-            get_body (Uri.of_string query) >|= fun s ->
-            match find_h1 s, find_search s with
-            | Some (em,title), _ ->
-              Some (Printf.sprintf "%s: %s (%s)" em title query)
-            | None, Some href ->
-              Some href
-            | None, None -> Some (Printf.sprintf "not found"))
-         (fun e ->
-            Log.err (fun k->k "emoji: query failed:@.%s" (Printexc.to_string e));
-            Lwt.return None))
+       find_emoji s)
 
 let plugin =
   [ cmd_yt;
