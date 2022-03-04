50053d948e9dd1a8178461241fa2ae608e40d7c8
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-03-04 00:15:53 -0500

    migrate state: special migration for factoids
    close #41

--- src/tools/migrate_state.ml
+++ src/tools/migrate_state.ml
@@ -1,5 +1,6 @@
 
 module DB = Sqlite3
+module J = Yojson.Safe
 
 let help=
   {|migrate_state state.json [opt]*
@@ -32,24 +33,60 @@ let () =
        UNIQUE (name) ON CONFLICT FAIL
        );
     |} |> check_db_;
+
   Printf.printf "creating index\n";
   DB.exec db {|
     CREATE INDEX IF NOT EXISTS plugins_idx on plugins(name);
     |} |> check_db_;
+
+  let tr_plugin (n,sub) =
+    Printf.printf "inserting %s\n%!" n;
+    if n="factoids" then (
+
+      DB.exec db {|
+        CREATE TABLE IF NOT EXISTS
+          factoids (
+            key TEXT NOT NULL,
+            value TEXT NOT NULL,
+            UNIQUE (key) on CONFLICT FAIL
+          );
+        |} |> check_db_;
+
+      DB.exec db {|
+        CREATE INDEX IF NOT EXISTS factoids_idx on factoids(key);
+      |} |> check_db_;
+
+      let l = J.Util.to_assoc sub in
+      let stmt = DB.prepare db {| INSERT INTO factoids(key,value) VALUES (?,?); |} in
+      List.iter
+        (fun (key, value) ->
+           let str_val = J.to_string value in
+
+           DB.reset stmt |> check_db_;
+           DB.bind_text stmt 1 key |> check_db_;
+           DB.bind_text stmt 2 str_val |> check_db_;
+           DB.step stmt |> check_db_;
+        ) l;
+
+      DB.finalize  stmt |> check_db_;
+
+
+    ) else (
+      let sub = J.to_string sub in
+      let stmt = DB.prepare db "INSERT INTO plugins(name,value) VALUES (?,?);" in
+      DB.bind_text stmt 1 n |> check_db_;
+      DB.bind_text stmt 2 sub |> check_db_;
+      DB.step stmt |> check_db_;
+      DB.finalize stmt |> check_db_;
+    )
+  in
+
   let j = Yojson.Safe.from_file !file in
   begin match j with
     | `Assoc l ->
       DB.exec db "BEGIN;" |> check_db_;
 
-      let stmt = DB.prepare db "INSERT INTO plugins(name,value) VALUES (?,?);" in
-      List.iter (fun (n,sub) ->
-          Printf.printf "inserting %s\n%!" n;
-          let sub = Yojson.Safe.to_string sub in
-          DB.reset stmt |> check_db_;
-          DB.bind_text stmt 1 n |> check_db_;
-          DB.bind_text stmt 2 sub |> check_db_;
-          DB.step stmt |> check_db_;
-        ) l;
+      List.iter tr_plugin l;
 
       Printf.printf "commit\n%!";
       DB.exec db "COMMIT;" |> check_db_;
